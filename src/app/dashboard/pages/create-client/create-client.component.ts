0ff67682823e380f1f7aadeaa9751ef23fba7055
Alonso <marenas@itsmarts.com.mx>
2024-07-10 14:45:20 -0600

    Start with structure of the proyect, 10/07

---
+++ src/app/dashboard/pages/create-client/create-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-client.component.html',
+  styles: ``
+})
+export class CreateClientComponent {
+
+}
---
+++ src/app/dashboard/pages/update-client/update-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-client.component.html',
+  styles: ``
+})
+export class UpdateClientComponent {
+
+}
